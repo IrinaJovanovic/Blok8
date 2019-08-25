c41c7521f7aabfe3d203f9bcd3ab528702c92681
IrinaJovanovic <irina96.jovanovic@gmail.com>
2019-08-25 20:26:29 +0200

    komponentne Home i Schedule
    Aleksandrin komit

---
+++ WebApp (2)/AngularApp/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+
+const routes: Routes = [ {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "schedule",
+    component: ScheduleComponent
+  },];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
