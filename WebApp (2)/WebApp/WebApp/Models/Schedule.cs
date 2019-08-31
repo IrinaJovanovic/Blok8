6d1aaf346440367a6c7500d99a30ad822c202085
IrinaJovanovic <irina96.jovanovic@gmail.com>
2019-08-31 14:00:02 +0200

    Schedule i CheckIn Controller(Aleksandra)

---
+++ WebApp (2)/WebApp/WebApp/Models/Schedule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using static WebApp.Models.Enums;
+
+namespace WebApp.Models.HelpModels
+{
+    public class Schedule
+    {
+        public int Id { get; set; }
+        public Day Day { get; set; }
+
+        public int LineId { get; set; }
+        public Line Line { get; set; }
+        public virtual List<Depature> Depatures { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        public double Version { get; set; }
+    }
+}
